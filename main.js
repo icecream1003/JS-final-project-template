a5cd8a5c7e70b2ef88964b211ac40020449685cd
icecream1003 <lineric102@gmail.com>
2016-09-04 11:59:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+var slime = {
+  x:0;
+  y:0;
+}
 function draw(){
   ctx.drawImage(bgImg,0,0);
-  ctx.drawImage(enemyImg,0,0);
+  ctx.drawImage(enemyImg,slime.x,slime.y);
 }
 setTimeout(draw,1000);
