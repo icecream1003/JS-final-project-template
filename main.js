79207e30b680fbba963becb11817dd135f2f14a3
icecream1003 <lineric102@gmail.com>
2016-09-04 11:41:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var enemy = document.createElement("img");
+enemy.src = "images/slime.gif";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
