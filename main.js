d40114be581c3f129951dae8709bfe28cd9d1659
icecream1003 <lineric102@gmail.com>
2016-09-04 11:43:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
-var enemy = document.createElement("img");
-enemy.src = "images/slime.gif";
+var enemyImg = document.createElement("img");
+enemyImg.src = "images/slime.gif";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(enemyImg,0,0);
 }
 setTimeout(draw,1000);
