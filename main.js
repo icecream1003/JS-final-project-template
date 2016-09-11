a35400a3c4cb713a7d72fbefa419d9997ea9ac12
icecream1003 <lineric102@gmail.com>
2016-09-11 10:57:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower-btn.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 var slime = {
@@ -11,5 +13,6 @@ var slime = {
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(enemyImg,slime.x,slime.y);
+  ctx.drawImage(towerImg,608,448);
 }
 setInterval(draw,16);
