fd1b47be32fb3477e7a89ae9eb86bc41195072fd
icecream1003 <lineric102@gmail.com>
2016-09-11 12:41:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,11 @@ var canvas = document.getElementById("game-canvas");
 
 var ctx = canvas.getContext("2d");
 
+var tower = {
+  x:0,
+  y:0,
+};
+
 var cursor = {
   x:0,
   y:0,
@@ -37,12 +42,15 @@ $("#game-canvas").on("click",function(){
 $("#game-canvas").on("mousemove",function(event){
   cursor.x = event.offsetX;
   cursor.y = event.offsetY;
+  tower.x = event.offsetX;
+  tower.y = event.offsetY;
 });
 
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(enemyImg,slime.x,slime.y);
   ctx.drawImage(towerbtnImg,590,430,50,50);
+  ctx.drawImage(towerImg,tower.x,tower.y)
   if(isBuilding == true){
     ctx.drawImage(towerImg,cursor.x,cursor.y);
   }
