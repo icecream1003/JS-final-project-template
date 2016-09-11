a82a2fd283695cb8b96e525aa6201d4738c0950c
icecream1003 <lineric102@gmail.com>
2016-09-11 11:31:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
 var towerbtnImg = document.createElement("img");
 towerbtnImg.src = "images/tower-btn.png";
+var cursor = {
+  x:0,
+  y:0,
+};
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 var slime = {
   x:0,
   y:0,
 };
+$("#game-canvas").mousemove(function(event){
+  console.log("x:" + event.offsetX+",y:"+eventoffsetY);
+});
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(enemyImg,slime.x,slime.y);
